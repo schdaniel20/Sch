b6d03ae0bdeb167eda26ab2812bad9e631ef847b
schdaniel20 <schdaniel20@gmail.com>
2016-06-22 10:27:53 +0300

    new

---
+++ Hello.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: schmithd
+ * Date: 22.06.2016
+ * Time: 10:26
+ */
+
+namespace Sch;
+
+class Hello {
+    public  function __construct()
+    {
+        echo "hello";
+    }
+}
